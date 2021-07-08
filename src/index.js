17f39622df8be369bacd7e3f960a5970d1f68858
SergeRomanenko13 <serhiiromanenko13@gmail.com>
2021-07-08 21:15:08 +0300

    1st commit

--- src/index.js
+++ src/index.js
@@ -26,19 +26,23 @@ const Student = function (name, surname, birthYear) {
         return new Date().getFullYear() - birthYear;
     };
 
-    this.arrayWithVisits = [];
-    this.arrayWithVisits.length = ARRAY_LENGTH;
-    this.arrayWithMarks = [];
-    this.arrayWithMarks.length = ARRAY_LENGTH;
+    this.arrayWithVisits = new Array(ARRAY_LENGTH);
+    this.arrayWithMarks = new Array(ARRAY_LENGTH);
 
     this.getAverageMark = function () {
+        let sum = 0;
+        for (const item of this.arrayWithMarks) {
+            if (item !== undefined) {
+                sum++;
+            }
+        }
         let total;
         try {
             total = this.arrayWithMarks.reduce((accumulator, currentValue) => accumulator + currentValue);
         } catch (error) {
             throw new Error('too few marks');
         }
-        return (total / this.arrayWithMarks.length).toFixed(ROUND_TO_ONE);
+        return (total / sum).toFixed(ROUND_TO_ONE);
     };
 
     this.present = function () {
@@ -75,13 +79,20 @@ const Student = function (name, surname, birthYear) {
 
     this.summary = function () {
         let visits = ZERO;
+        let sum = 0;
+        for (const item of this.arrayWithVisits) {
+            if (item !== undefined) {
+                sum++;
+            }
+        }
         this.arrayWithVisits.forEach(elem => {
             if (elem === true) {
                 visits++;
             }
             return visits;
         });
-        const averageVisits = visits / this.arrayWithVisits.length;
+
+        const averageVisits = visits / sum;
         if (this.getAverageMark() >= COMPARISON_NUMBER_1 && averageVisits >= COMPARISON_NUMBER_2) {
             return 'Ути какой молодчинка!';
         } else if (this.getAverageMark() < COMPARISON_NUMBER_1 && averageVisits < COMPARISON_NUMBER_2) {
@@ -109,13 +120,12 @@ oleh.abscent();
 oleh.mark(TEN);
 oleh.mark(TEN);
 oleh.mark(TEN);
-oleh.mark(TEN);
-oleh.mark(TEN);
 oleh.mark(SEVEN);
 oleh.mark(TEN);
 oleh.mark(TEN);
 oleh.mark(TEN);
 oleh.mark(NINE);
+ivan.mark(NINE);
 ivan.mark(NINE);
 petro.mark(NINE);
 // console.log('average mark of Oleh is: ', oleh.getAverageMark());
